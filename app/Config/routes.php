ecc084ef237e5dbf11502853197eb19794da49b5
luisdias <luis.edudias@gmail.com>
2017-06-19 14:42:19 +0100

    Enhacement
    new default page

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -13,14 +13,14 @@
 
  // routes.php
 
- 
+
  
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/', array('controller' => 'panel', 'action' => 'index'));	
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
