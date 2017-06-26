7cb8e8f252f13919296a07aa0927bbda2d84163f
luisdias <luis.edudias@gmail.com>
2017-06-26 20:32:55 +0100

    NEW: reset route

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -20,7 +20,8 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'panel', 'action' => 'index'));	
+	Router::connect('/', array('controller' => 'panel', 'action' => 'index'));
+	Router::connect('/reset/*',array('controller'=>'Users','action'=>'reset'));		
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
